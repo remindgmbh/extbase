2101545dbd6417138577cad7039211a36562fa15
David Mellen <d.mellen@remind.de>
2022-12-01 13:34:34 +0100

    Fix iterating over non existing array in flexFormHook

--- Classes/Hooks/FlexFormHook.php
+++ Classes/Hooks/FlexFormHook.php
@@ -13,38 +13,42 @@ class FlexFormHook
     public function parseDataStructureByIdentifierPostProcess(array $dataStructure, array $identifier): array
     {
         if (isset($dataStructure['sheets'][ListFiltersSheets::BACKEND_SHEET_ID])) {
-            foreach ($dataStructure['sheets'][ListFiltersSheets::BACKEND_SHEET_ID]['ROOT']['el'] as $key => &$value) {
-                if (str_starts_with($key, 'settings.' . ListFiltersSheets::BACKEND_FILTERS)) {
-                    $label = $this->getLabel($value['label']);
-                    if (str_ends_with($key, '.' . ListFiltersSheets::CONJUNCTION)) {
-                        $value['label'] = LocalizationUtility::translate(
-                            'filters.backend.conjunction',
-                            'rmnd_extbase',
-                            [$label]
-                        );
-                    }
-                    if (str_ends_with($key, '.' . ListFiltersSheets::VALUES)) {
-                        $value['label'] = LocalizationUtility::translate(
-                            'filters.backend.values',
-                            'rmnd_extbase',
-                            [$label]
-                        );
+            if (is_array($dataStructure['sheets'][ListFiltersSheets::BACKEND_SHEET_ID]['ROOT']['el'])) {
+                foreach ($dataStructure['sheets'][ListFiltersSheets::BACKEND_SHEET_ID]['ROOT']['el'] as $key => &$value) {
+                    if (str_starts_with($key, 'settings.' . ListFiltersSheets::BACKEND_FILTERS)) {
+                        $label = $this->getLabel($value['label']);
+                        if (str_ends_with($key, '.' . ListFiltersSheets::CONJUNCTION)) {
+                            $value['label'] = LocalizationUtility::translate(
+                                'filters.backend.conjunction',
+                                'rmnd_extbase',
+                                [$label]
+                            );
+                        }
+                        if (str_ends_with($key, '.' . ListFiltersSheets::VALUES)) {
+                            $value['label'] = LocalizationUtility::translate(
+                                'filters.backend.values',
+                                'rmnd_extbase',
+                                [$label]
+                            );
+                        }
                     }
                 }
             }
         }
         if (isset($dataStructure['sheets'][ListFiltersSheets::FRONTEND_SHEET_ID])) {
-            foreach ($dataStructure['sheets'][ListFiltersSheets::FRONTEND_SHEET_ID]['ROOT']['el'] as $key => &$value) {
-                if (
-                    str_starts_with($key, 'settings.' . ListFiltersSheets::FRONTEND_FILTERS) &&
-                    str_ends_with($key, '.' . ListFiltersSheets::VALUES)
-                ) {
-                    $label = $this->getLabel($value['label']);
-                    $value['label'] = LocalizationUtility::translate(
-                        'filters.frontend.values',
-                        'rmnd_extbase',
-                        [$label]
-                    );
+            if (is_array($dataStructure['sheets'][ListFiltersSheets::FRONTEND_SHEET_ID]['ROOT']['el'])) {
+                foreach ($dataStructure['sheets'][ListFiltersSheets::FRONTEND_SHEET_ID]['ROOT']['el'] as $key => &$value) {
+                    if (
+                        str_starts_with($key, 'settings.' . ListFiltersSheets::FRONTEND_FILTERS) &&
+                        str_ends_with($key, '.' . ListFiltersSheets::VALUES)
+                    ) {
+                        $label = $this->getLabel($value['label']);
+                        $value['label'] = LocalizationUtility::translate(
+                            'filters.frontend.values',
+                            'rmnd_extbase',
+                            [$label]
+                        );
+                    }
                 }
             }
         }
