729f833eb5e9b068df291f5551c7bfcc4d1bec9c
David Mellen <d.mellen@remind.de>
2023-01-11 11:02:11 +0100

    Add QueryExtbasePluginEnhancer (moved from rmnd_headless) and PersistedValueMapper

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Remind\Extbase\Backend\Form\Container\FlexFormContainerContainer;
 use Remind\Extbase\Backend\Form\Element\FilterAppliedValuesElement;
 use Remind\Extbase\Backend\Form\Element\FilterAvailableValuesElement;
+use Remind\Extbase\Routing\Aspect\PersistedValueMapper;
+use Remind\Extbase\Routing\Enhancer\QueryExtbasePluginEnhancer;
 use TYPO3\CMS\Backend\Form\Container\FlexFormContainerContainer as BaseFlexFormContainerContainer;
 
 defined('TYPO3_MODE') || die('Access denied.');
@@ -25,4 +27,18 @@ defined('TYPO3_MODE') || die('Access denied.');
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][BaseFlexFormContainerContainer::class] = [
         'className' => FlexFormContainerContainer::class,
     ];
+
+    $GLOBALS
+        ['TYPO3_CONF_VARS']
+        ['SYS']
+        ['routing']
+        ['aspects']
+        ['PersistedValueMapper'] = PersistedValueMapper::class;
+
+    $GLOBALS
+        ['TYPO3_CONF_VARS']
+        ['SYS']
+        ['routing']
+        ['enhancers']
+        ['QueryExtbase'] = QueryExtbasePluginEnhancer::class;
 })();
