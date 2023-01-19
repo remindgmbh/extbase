dd65a2f6b5a9824ab987d9d895bc4cc348b2fb0b
David Mellen <d.mellen@remind.de>
2023-01-19 14:29:10 +0100

    Allow aspects for filter parameters
    RETE-1

--- Classes/Routing/Aspect/FilterValueMapper.php
+++ Classes/Routing/Aspect/FilterValueMapper.php
@@ -4,52 +4,123 @@ declare(strict_types=1);
 
 namespace Remind\Extbase\Routing\Aspect;
 
+use InvalidArgumentException;
 use Remind\Extbase\Utility\FilterUtility;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Core\Routing\Aspect\PersistedAliasMapper;
+use TYPO3\CMS\Core\Context\ContextAwareInterface;
+use TYPO3\CMS\Core\Context\ContextAwareTrait;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Database\Query\Restriction\FrontendGroupRestriction;
+use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
+use TYPO3\CMS\Core\Routing\Aspect\AspectFactory;
+use TYPO3\CMS\Core\Routing\Aspect\MappableAspectInterface;
+use TYPO3\CMS\Core\Routing\Aspect\ModifiableAspectInterface;
+use TYPO3\CMS\Core\Routing\Aspect\SiteAccessorTrait;
+use TYPO3\CMS\Core\Site\SiteAwareInterface;
+use TYPO3\CMS\Core\Site\SiteLanguageAwareInterface;
+use TYPO3\CMS\Core\Site\SiteLanguageAwareTrait;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class FilterValueMapper extends PersistedAliasMapper
+class FilterValueMapper implements
+    MappableAspectInterface,
+    ContextAwareInterface,
+    SiteLanguageAwareInterface,
+    SiteAwareInterface
 {
+    use ContextAwareTrait;
+    use SiteLanguageAwareTrait;
+    use SiteAccessorTrait;
+
+    private string $tableName;
+    private array $parameters;
+    private array $settings;
+    private AspectFactory $aspectFactory;
+
     public function __construct(array $settings)
     {
-        $settings['routeFieldName'] = '';
-        parent::__construct($settings);
+        $tableName = $settings['tableName'] ?? null;
+        $parameters = $settings['parameters'] ?? [];
+
+        if (!is_string($tableName)) {
+            throw new InvalidArgumentException(
+                'tableName must be string',
+                1674134308
+            );
+        }
+
+        if (!is_array($parameters)) {
+            throw new InvalidArgumentException(
+                'parameters must be array',
+                1674134532
+            );
+        }
+
+        $this->settings = $settings;
+        $this->tableName = $tableName;
+        $this->parameters = $parameters;
+        $this->aspectFactory = GeneralUtility::makeInstance(AspectFactory::class);
     }
 
     /**
      * @param string $value
      * @return null|string
      */
-    public function generate(string $value): ?string
+    public function generate(string $originalValue): ?string
     {
-        return $this->mapValue($value);
+        $originalParameters = $this->prepareParameters($originalValue);
+
+        if (!$this->validateValues($originalParameters)) {
+            return null;
+        }
+
+        $result = [];
+        $parametersMap = $this->getParametersMap();
+
+        foreach ($originalParameters as $field => $values) {
+            $field = $parametersMap[$field] ?? $field;
+            $result[$field] = $values;
+        }
+
+        return json_encode($result);
     }
 
     /**
      * @param string $value
      * @return null|string
      */
-    public function resolve(string $value): ?string
+    public function resolve(string $originalValue): ?string
     {
-        return $this->mapValue($value);
+        $originalParameters = $this->prepareParameters($originalValue);
+
+        $result = [];
+        $parametersMap = array_flip($this->getParametersMap());
+
+        foreach ($originalParameters as $field => $values) {
+            $field = $parametersMap[$field] ?? $field;
+            $result[$field] = $values;
+        }
+
+        if (!$this->validateValues($result)) {
+            return null;
+        }
+
+        return json_encode($result);
     }
 
-    private function mapValue(string $originalValue): ?string
+    private function validateValues(array $parameters): bool
     {
-        $parameters = json_decode($originalValue, true);
-        $normalizedParameters = FilterUtility::normalizeQueryParameters($parameters);
-
-        foreach ($normalizedParameters as $field => $values) {
+        foreach ($parameters as $field => $values) {
             foreach ($values as $value) {
-                if (!$this->exists($field, $value)) {
-                    return null;
+                if (!$this->validateValue($field, $value)) {
+                    return false;
                 }
             }
         }
-        return $originalValue;
+        return true;
     }
 
-    private function exists(string $fieldName, string $value): bool
+    private function validateValue(string $fieldName, string $value): bool
     {
         $fieldTca = BackendUtility::getTcaFieldConfiguration($this->tableName, $fieldName);
         $mmTable = $fieldTca['MM'] ?? null;
@@ -85,5 +156,51 @@ class FilterValueMapper extends PersistedAliasMapper
 
         $queryResult = $queryBuilder->executeQuery();
         return $queryResult->rowCount() > 0;
+    }
+
+    private function getParametersMap(): array
+    {
+        $result = [];
+        foreach ($this->parameters as $fieldName => $parameter) {
+            if (is_string($parameter)) {
+                $result[$fieldName] = $parameter;
+            } else {
+                $aspects = $this->aspectFactory->createAspects(
+                    [$parameter],
+                    $this->getSiteLanguage(),
+                    $this->getSite()
+                );
+                $modifier = current($aspects);
+                if (!($modifier instanceof ModifiableAspectInterface)) {
+                    throw new InvalidArgumentException(
+                        sprintf(
+                            'FilterValueMapper _arguments aspects must be of type \'%s\'',
+                            ModifiableAspectInterface::class
+                        ),
+                        1674134317
+                    );
+                }
+                $result[$fieldName] = $modifier->modify();
+            }
+        }
+        return $result;
+    }
+
+    private function prepareParameters(string $value): array
+    {
+        $parameters = json_decode($value, true);
+        return FilterUtility::normalizeQueryParameters($parameters);
+    }
+
+    private function createQueryBuilder(): QueryBuilder
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable($this->tableName)
+            ->from($this->tableName);
+        $queryBuilder->setRestrictions(
+            GeneralUtility::makeInstance(FrontendRestrictionContainer::class, $this->context)
+        );
+        $queryBuilder->getRestrictions()->removeByType(FrontendGroupRestriction::class);
+        return $queryBuilder;
     }
 }
