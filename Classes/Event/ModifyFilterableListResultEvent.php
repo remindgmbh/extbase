8c2559f4d6ac9f3860428c7bbe9955ee1fcdc3af
Dominik Lehrl <d.lehrl@remind.de>
2026-02-09 10:33:58 +0100

    fix: pass UriBuilder to ModifyFilterableListResultEvent

--- Classes/Event/ModifyFilterableListResultEvent.php
+++ Classes/Event/ModifyFilterableListResultEvent.php
@@ -5,17 +5,22 @@ declare(strict_types=1);
 namespace Remind\Extbase\Event;
 
 use Remind\Extbase\Controller\Dto\FilterableListResult;
+use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 
 final class ModifyFilterableListResultEvent extends AbstractExtbaseEvent
 {
     private FilterableListResult $filterableListResult;
 
+    private UriBuilder $uriBuilder;
+
     public function __construct(
         string $extensionName,
         FilterableListResult $filterableListResult,
+        UriBuilder $uriBuilder,
     ) {
         parent::__construct($extensionName);
         $this->filterableListResult = $filterableListResult;
+        $this->uriBuilder = $uriBuilder;
     }
 
     public function getFilterableListResult(): FilterableListResult
@@ -28,5 +33,10 @@ final class ModifyFilterableListResultEvent extends AbstractExtbaseEvent
         $this->filterableListResult = $filterableListResult;
 
         return $this;
+    }
+
+    public function getUriBuilder(): UriBuilder
+    {
+        return $this->uriBuilder;
     }
 }
